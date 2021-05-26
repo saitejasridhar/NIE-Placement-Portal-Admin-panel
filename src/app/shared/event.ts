be800145e8110bc1a214f065953f0ff1c7cdebde
Sai Teja N <saitejan@Sais-MacBook-Pro.local>
2021-05-27 00:31:30 +0530

    CRUD Announcments

--- src/app/shared/event.ts
+++ src/app/shared/event.ts
@@ -1,4 +1,4 @@
 export class Event {
-    title: string;
-    description:string;
+    Name: string;
+    Description:string;
 }
---
+++ src/app/shared/services/events.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
+import {Event} from '../event';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class EventService {
+  companysRef: AngularFireList<any>;
+  companyRef: AngularFireObject<any>;
+
+  constructor(private db: AngularFireDatabase, private firestore: AngularFirestore) {}
+
+
+AddEvent(event:Event){ 
+  return new Promise<any>((resolve, reject) => { 
+     this.firestore
+         .collection("Announcments")
+         .add(event)
+         .then(
+             res => {}, 
+             err => alert(err.message)
+         )
+  }
+)}
+
+
+  /* Get company */
+  GetEvent(id: string) {
+    const employeeData= this.firestore.collection("Announcments").doc(id).valueChanges();
+    return employeeData;
+  }  
+
+  /* Get company list */
+  GetEventList() {
+    return this.firestore.collection("Announcments").snapshotChanges();
+  }
+
+
+  /* Update company */
+  UpdateEvent(id, company: Event) {
+    return this.firestore.doc('Announcments/' + id).set({
+    Name:company.Name,
+    Description:company.Description,
+  
+  }).catch(error => {
+      this.errorMgmt(error);
+    })
+  }
+
+
+  /* Delete company */
+  DeleteEvent(id: string) {
+    return this.firestore.doc('Announcments/' + id).delete();
+  }
+
+  // Error management
+  private errorMgmt(error) {
+    console.log(error)
+  }
+}
